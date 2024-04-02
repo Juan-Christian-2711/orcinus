91ffd6face42f1dc63d96d0227029cb47dccc547
Juan Christian <Juanchristian2002@outlook.com>
2024-04-02 20:50:40 +0100

    Setting up report and improving formatting

--- orcinus/orcinus.py
+++ orcinus/orcinus.py
@@ -1,20 +1,27 @@
 from manifest import Manifest
 from api import Api
-class Main:
+from report import Report
+
+class orcinus:
     def __init__(self):
         # Constructor
         pass
     def run(self):
         manifest = Manifest()
         index = manifest.index()
+        
         service = list(index["services"].keys())[0]
         version = index["version"]
-        cpe_name = "cpe:2.3:a:{}:{}:{}:*:*:*:-:*:*:*".format(service,service, version)
-        api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={}".format(cpe_name)
+        
+        cpe_name = f"cpe:2.3:a:{service}:{service}:{version}:*:*:*:-:*:*:*"         
+        api_url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={cpe_name}"
+        
         api = Api()
         data = api.connect_to_api(api_url)
-        print(data)
+        
+        api_report = Report(data)
+        api_report.printReport()
 
 if __name__ == "__main__":
-    main_instance = Main()
+    main_instance = orcinus()
     main_instance.run()
---
+++ orcinus/report.py
@@ -0,0 +1,6 @@
+class Report:
+    def __init__(self, data) -> None:
+        self.data = data
+    def printReport(self) -> None:
+        print(self.data)
+        

097bd0bdde1a960778cdee9f764c4c8b6115495d
Juan Christian <Juanchristian2002@outlook.com>
2024-04-03 15:21:45 +0100

    changing the output of the application based on the chosen arguments

--- orcinus/report.py
+++ orcinus/report.py
@@ -1,6 +1,13 @@
+import json
+
 class Report:
     def __init__(self, data) -> None:
         self.data = data
-    def printReport(self) -> None:
-        print(self.data)
-        
+    def printReport(self, option) -> None:
+        if (option == "reportCveCount"):
+            print(self.data['totalResults'])
+        if (option == "reportCveIds"):
+            for i in self.data['vulnerabilities']:
+                for key, value in i.items():
+                    print(value.keys())
+                    print(value['id'])
